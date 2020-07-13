c7309df56e40f8051aed89e5e6860b84c64d278b
VR314 <54733121+VR314@users.noreply.github.com>
2020-07-12 22:27:22 -0400

    edited README.md removed overlay.js

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+This is a [Next.js](https://nextjs.org/) project used to provide data on the state of the COVID-19 pandemic in each of the states of the US.
 
 ## Getting Started
-
-Clone this repository and run the development server:
+Clone this repository and run the development server to run + edit on your local machine:
 
 ```bash
-npm run dev
+npm install #installs all dependencies from pacakge.json
+# or
+yarn install
+
+npm run dev #starts development server
 # or
 yarn dev
 ```
 
-Icon credits: [iconfinder](https://www.iconfinder.com/iconsets/coronavirus-flat)
+Vercel serverless functions (in the /api folder)[/api] are used to process data server-side, reading data from [the Covid Tracking Project](https://covidtracking.com/data/api). 
+
+Icon credits: 
+- [Iconfinder](https://www.iconfinder.com/iconsets/coronavirus-flat)
+- [SimpleIcos](https://simpleicons.org/)
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
-import Overlay from '../components/overlay'
-import cookie from 'react-cookie';
+//import cookie from 'react-cookie';
 import Link from 'next/link'
 
 const states = { "AZ": "Arizona", "AL": "Alabama", "AK": "Alaska", "AR": "Arkansas", "CA": "California", "CO": "Colorado", "CT": "Connecticut", "DC": "DC", "DE": "Delaware", "FL": "Florida", "GA": "Georgia", "HI": "Hawaii", "ID": "Idaho", "IL": "Illinois", "IN": "Indiana", "IA": "Iowa", "KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana", "ME": "Maine", "MD": "Maryland", "MA": "Massachusetts", "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi", "MO": "Missouri", "MT": "Montana", "NE": "Nebraska", "NV": "Nevada", "NH": "New Hampshire", "NJ": "New Jersey", "NM": "New Mexico", "NY": "New York", "NC": "North Carolina", "ND": "North Dakota", "OH": "Ohio", "OK": "Oklahoma", "OR": "Oregon", "PA": "Pennsylvania", "RI": "Rhode Island", "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee", "TX": "Texas", "UT": "Utah", "VT": "Vermont", "VA": "Virginia", "WA": "Washington", "WV": "West Virginia", "WI": "Wisconsin", "WY": "Wyoming" };
 
 export default function Home() {
-  //const [cookies, setCookies] = useState(cookie.load('location'));
   var links = []
   for (var state in states) {
-    links.push((<div className="column"><div className="box has-text-centered"><Link href={"/states/" + state.toLowerCase()}><a>{states[state]}</a></Link>
-      <br /></div></div>));
+    links.push((<div className="column"><div className="box has-text-centered"><Link href={"/states/" + state.toLowerCase()}><a>{states[state]}</a></Link></div></div>));
   }
 
   return (
@@ -23,7 +20,7 @@ export default function Home() {
           <link rel="icon" href="/corona-nb.ico" />
         </Head>
         <body className="container">
-          <h1 className="title is-1 has-text-white-ter has-text-centered">
+          <h1 className="title is-1 has-text-white-ter has-text-centered mx-6">
             Welcome to Active Cases! Choose a state:
           </h1>
           <div className="columns is-mobile is-multiline">
